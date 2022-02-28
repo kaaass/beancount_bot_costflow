735ed5dbaac07c25336ecd5559addae2799a9a13
KAAAsS <admin@kaaass.net>
2022-02-28 16:26:01 +0800

    Feat: check node environment when start

--- beancount_bot_costflow/beancount_bot_costflow.py
+++ beancount_bot_costflow/beancount_bot_costflow.py
@@ -7,6 +7,7 @@ from beancount_bot.util import logger
 
 _PATH = os.path.split(os.path.realpath(__file__))[0]
 _SCRIPT_PATH = os.path.join(_PATH, 'costflow-parser.js')
+_NODE_MIN_MAJOR = 14
 
 
 class CostflowDispatcher(Dispatcher):
@@ -37,6 +38,7 @@ class CostflowDispatcher(Dispatcher):
 
     def __init__(self, costflow_config: str):
         self.config = costflow_config
+        self.check_environment()
 
     def quick_check(self, input_str: str) -> bool:
         # 基本什么都可以接受
@@ -51,10 +53,27 @@ class CostflowDispatcher(Dispatcher):
                  input_str], stderr=subprocess.STDOUT, timeout=3,
                 universal_newlines=True)
         except subprocess.CalledProcessError as e:
-            logger.error(f'Costflow 解析错误：{e.output}', e)
+            logger.error(f'Costflow 解析错误：%s。%s', e.output, e)
             raise ValueError('Costflow 解析错误，请检查相关配置！')
         logger.debug("调用 Costflow CLI 返回：%s", output)
         data = json.loads(output)
         if 'error' in data:
             raise ValueError(f'Costflow 错误：{data["error"]}')
         return data['output']
+
+    def check_environment(self):
+        """
+        启动时检查环境
+        """
+        # Node 检查
+        try:
+            output = subprocess.check_output(
+                ['node', '-v'], stderr=subprocess.STDOUT, timeout=3,
+                universal_newlines=True)
+            major_version = int(output[1:].split('.')[0])
+            if major_version < _NODE_MIN_MAJOR:
+                logger.error(f'Node 版本过低，最低版本需要 {_NODE_MIN_MAJOR}，当前版本 {output}')
+                raise EnvironmentError(f'Node 版本过低，最低版本需要 {_NODE_MIN_MAJOR}，当前版本 {output}')
+        except FileNotFoundError as e:
+            logger.error('Node 调用错误，请检查是否安装 Node >= %s！%s', _NODE_MIN_MAJOR, e)
+            raise EnvironmentError(f'Node 调用错误，请检查是否安装 Node >= {_NODE_MIN_MAJOR}！')
